402b393fc359f8d84aec571170ab7c95eb23e52c
ocean-gao <gaopandyx@163.com>
2024-11-22 15:50:20 +0800

    refactor: docker 安装 redis 目录调整

--- packages/docs/.vitepress/configs/sidebar/backend.ts
+++ packages/docs/.vitepress/configs/sidebar/backend.ts
@@ -12,6 +12,10 @@ export default [
         link: '/backend/20240109_浅谈《凤凰架构》/',
     },
     {
+        text: 'docker 安装 redis',
+        link: '/backend/20230705_docker安装redis/',
+    },
+    {
         text: '数据库',
         collapsed: false,
         items: [
@@ -26,10 +30,6 @@ export default [
             {
                 text: '常见 MySQL 报错',
                 link: '/backend/20210412_数据库/20230704_常见MySQL报错/',
-            },
-            {
-                text: 'docker 安装 redis',
-                link: '/backend/20210412_数据库/20230705_docker安装redis/',
             },
         ],
     },
