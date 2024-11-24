d0794e31311a1aeff200d8b56b0e87bd0714767b
ocean_gao <gaopandyx@163.com>
2024-11-25 01:33:27 +0800

    feat: 理解浏览器的缓存机制

--- packages/docs/.vitepress/configs/sidebar/operation.ts
+++ packages/docs/.vitepress/configs/sidebar/operation.ts
@@ -15,4 +15,8 @@ export default [
         text: 'CDN 原理简单介绍',
         link: '/operation/20210529_CDN原理简单介绍/',
     },
+    {
+        text: '理解浏览器的缓存机制',
+        link: '/operation/20210601_理解浏览器的缓存机制/',
+    },
 ];
